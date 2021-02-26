d2f640de25c7942ae36a6d6963aed0a85700d2b2
mingyeongmo <mingyeongmp@naver.com>
2021-02-26 22:45:27 +0900

    자바 공부

--- JS_std/function.js
+++ JS_std/function.js
@@ -48,5 +48,131 @@ function printMessage(){
     let message = 'hello';
     console.log(message); // local variable
     console.log(globalMessage);
+    function printAnother() {
+        console.log(message);
+        let childMessage = 'hello';
+    }
 }
 printMessage();
+
+// 6. Return a value
+function sum(a,b){
+    return a+b;
+}
+const result = sum(1,2); // 3
+console.log(`sum: ${sum(1,2)}`);
+
+// 7. Early return, early exit
+// bad
+
+function upgradeUser(user) {
+    if (user.point > 10){
+        // long upgrade logic...
+    }
+}
+
+// good
+
+function upgradeUser(user) {
+    if (user.point <= 10){
+        return;
+    }
+    // long upgrade logic...
+}
+
+// First-class function
+// functions are treated like any other variable
+// can be assigned as a value to variable
+// can be passed as an argument to other functions.
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defiend. (hoisted)
+// a function expression is created when the execution reaches it.
+const print = function (){ // anonymous function
+    console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1,3));
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo){
+    if(answer==='love you'){
+        printYes();
+    }else{
+        printNo();
+    }
+}
+
+// anonymous function
+const printYes = function(){  
+    console.log('yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print(){ 
+    console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+// const simplePrint = function (){
+//     console.log('simplePrint!');
+// };
+
+const simplePrint = () => console.log('simplePrint!');
+const add = (a,b) => a+b;
+
+// const add = function(a,b){
+//     return a+b;
+// };
+
+// IIFE: Immediately Invoked Function Expression
+(function hello(){
+    console.log('IIFE');
+})();
+
+// function calculate(command, a, b)
+// command: add, substract, divide, mltiply, remainder
+// function add(a,b){
+//     return a+b;
+// }
+
+// function substract(a,b){
+//     return a-b;
+// }
+
+// function divide(a,b){
+//     return a/b;
+// }
+
+// function
+
+// function gae(a,b){
+
+// }
+
+function calculate(command, a, b){
+    switch (command){
+        case 'add':
+            return a+b;
+        case 'substract':
+            return a-b;
+        case 'divide':
+            return a/b;
+        case 'multiply':
+            return a*b;
+        case 'remainder':
+            return a%b;
+        default:
+            throw Error('unknown command')
+    }
+}
+console.log(calculate('add',2,3));
