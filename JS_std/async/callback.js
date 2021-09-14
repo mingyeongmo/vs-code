4d2fa9375d08750f343d376fcd801ed862a1b885
mingyeongmo <mingyeongmp@naver.com>
2021-09-14 19:53:29 +0900

    callback 지옥체험

--- JS_std/async/callback.js
+++ JS_std/async/callback.js
@@ -26,3 +26,55 @@ function printWithDelay(print, timeout) {
 // 함수 선언이 제일 위로 올라감(hoisting)
 
 printWithDelay(() => console.log('async callback'), 2000);
+
+// Callback 지옥 체험
+
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(()=> {
+            if(
+                (id === 'ellie' && password === 'dream') ||
+                (id === 'coder' && password === 'academy')
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(() => {
+            if (user === 'ellie') {
+                onSuccess({ name: 'ellie', role: 'admin' });
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id,
+    password,
+    (user) => {
+        userStorage.getRoles(
+            user,
+            (userWithRole) => {
+                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+            },
+            error => {
+                console.log(error);
+            }
+            );
+    },
+    (error) =>{
+        console.log(error);
+    }
+);
+
+// 가독성 떨어짐
+// 문제 분석 어려움 (ex 에러가 어디서 났는지 가늠하기 힘듦.) 
